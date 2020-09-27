27e2b42b2fe4e7d3c7a69d2008763e3823bc4825
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-09-26 22:18:40 -0700

    moved static files into static, and created app/static, changed settings.py to reflect same

--- aMagicalChildhood/settings.py
+++ aMagicalChildhood/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'app/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static/staticfiles')
+STATIC_URL = '/app/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
